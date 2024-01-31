7f265d0e1e891f38d9f78101c39943078e036b60
axai <02b3akshay@gmail.com>
2024-01-31 16:29:12 +0530

    Add files to git

---
+++ blog/info.py
@@ -0,0 +1,13 @@
+# for gmail smtp(which is not working currently)
+#EMAIL_USE_TLS = True
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_HOST_USER = 'mynameismiabab6@gmail.com'
+#EMAIL_HOST_PASSWORD = 'yoyo456789@'
+#EMAIL_PORT = 587
+
+# This is for microsoft outllook(currently this is working !)
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp-mail.outlook.com'
+EMAIL_HOST_USER = 'axaitest@outlook.com'
+EMAIL_HOST_PASSWORD = 'Prasad@feb20'
+EMAIL_PORT = 587
